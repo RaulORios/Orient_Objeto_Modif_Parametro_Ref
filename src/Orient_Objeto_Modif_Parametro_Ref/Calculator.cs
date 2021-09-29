95f5b1eb105b70bb79b965c9aa1d644e38d2bea4
RaulORios <raul.orios89@outlook.com>
2021-09-29 14:16:22 -0300

    Arquivos atualizados

---
+++ src/Orient_Objeto_Modif_Parametro_Ref/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orient_Objeto_Modif_Parametro_Ref
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ src/Orient_Objeto_Modif_Parametro_Ref/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Orient_Objeto_Modif_Parametro_Ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
